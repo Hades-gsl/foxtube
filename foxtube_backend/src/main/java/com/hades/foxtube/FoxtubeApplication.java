934842cfd58f89eb7513a70facb2b49297dd23a4
Tom <2748887019@qq.com>
2024-04-06 16:45:44 +0800

    init project

---
+++ foxtube_backend/src/main/java/com/hades/foxtube/FoxtubeApplication.java
@@ -0,0 +1,13 @@
+package com.hades.foxtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxtubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxtubeApplication.class, args);
+	}
+
+}
