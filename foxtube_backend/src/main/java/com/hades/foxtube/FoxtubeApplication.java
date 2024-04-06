bb7c17113f48d84f6177af42e05d29b6364bed41
Tom <2748887019@qq.com>
2024-04-06 17:06:31 +0800

    init comment about the class

--- foxtube_backend/src/main/java/com/hades/foxtube/FoxtubeApplication.java
+++ foxtube_backend/src/main/java/com/hades/foxtube/FoxtubeApplication.java
@@ -3,11 +3,14 @@ package com.hades.foxtube;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author: Hades
+ * @Date: 2024/4/6 16:52
+ * @Description: Main application class for the Foxtube project
+ */
 @SpringBootApplication
 public class FoxtubeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FoxtubeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FoxtubeApplication.class, args);
+    }
 }
