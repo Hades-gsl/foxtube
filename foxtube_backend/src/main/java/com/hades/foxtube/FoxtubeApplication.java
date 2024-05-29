baf7a5cee51d98db078d5649569ced4c9f98bae9
Tom <2748887019@qq.com>
2024-05-29 11:44:29 +0800

    add openapi file and generate code

--- foxtube_backend/src/main/java/com/hades/foxtube/FoxtubeApplication.java
+++ foxtube_backend/src/main/java/com/hades/foxtube/FoxtubeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoxtubeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoxtubeApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(FoxtubeApplication.class, args);
+  }
 }
